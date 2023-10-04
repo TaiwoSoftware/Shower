8af70cd4faa33172fb7d861d99f8790a0c896157
DamoFD <voshall@yahoo.com>
2023-10-04 03:32:20 -0700

    created authentication functionality
    created authentication functionality

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import '../../css/styles.css'
+import Header from "../Components/Header/Header";
+import Body from '../Components/Body/Body';
+import Footer from '../Components/Footer/Footer';
+const App = () => {
+    return ( 
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+     );
+}
+ 
+export default App;
