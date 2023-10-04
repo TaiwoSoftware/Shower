51afd0c2c460b208fbd4d5a23f0b86fe9837d131
Taiwo AKerele <davcodes24@gmail.com>
2023-10-04 10:27:43 +0100

    making it responsive

--- src/Components/Body/BodyMainTwo.jsx
+++ src/Components/Body/BodyMainTwo.jsx
@@ -1,6 +1,6 @@
 const BodyMainTwo = () => {
     return (
-        <div className="bodyMain">
+        <div className="bodyMain two">
             <p>Our aim is for people not to do mistake in life and basically be on the right track for children, young adult, youth not to make another mistake because we understand that some people are not given the opportunity to get an adviser so we developed it</p>
         </div>
     );
